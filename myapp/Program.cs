6c14eb1c58c704d4bc224982c77973bb1c30184d
odedwarem <odedwar@gmail.com>
2025-06-14 13:24:38 +0300

    .net app fix

--- myapp/Program.cs
+++ myapp/Program.cs
@@ -1,13 +1,17 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "MyApp API", Version = "v1" });
+});
 
 var app = builder.Build();
 
